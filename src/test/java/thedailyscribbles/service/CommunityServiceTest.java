5643cf6d764b33d0ebd3d27427f470e6bce508ae
xavierdavid5667 <xavierdavid5667@gmail.com>
2023-06-13 10:55:31 +0530

    made changes in properties

--- src/test/java/thedailyscribbles/service/CommunityServiceTest.java
+++ src/test/java/thedailyscribbles/service/CommunityServiceTest.java
@@ -1,6 +1,7 @@
 package thedailyscribbles.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
